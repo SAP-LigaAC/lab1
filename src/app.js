37c201e32f5d38cc7c4c2847c37a7f9da9d601f9
i343516 <darius.birescu@sap.com>
2022-04-04 14:40:35 +0300

    Initial commit for simple app

--- src/app.js
+++ src/app.js
@@ -1,46 +1,42 @@
 import bodyParser from 'body-parser';
 import express from 'express';
 import routes from './routes/bookingsRoutes';
-import {StatusCodes, ReasonPhrases} from 'http-status-codes';
+import { StatusCodes, ReasonPhrases } from 'http-status-codes';
+import res from 'express/lib/response';
+// import { addBooking } from './controllers/bookingsController';
 
+const bookings = [];
 
 export const initializeApp = () => {
 
     const app = express();
-
-    app.use(bodyParser.urlencoded({
-        extended: true
-    }));
+    // app.use(bodyParser.urlencoded({
+    //     extended: true
+    // }));
 
     app.use(bodyParser.json());
 
     app.get('/', (req, res) => res.send('App is working'));
+    app.get('/bookings', (req, res) => res.send(bookings));
+    app.post('/bookings', (req, res) => addBooking(req, res));
 
-    app.use('/api', routes);
 
-    app.use((req, res) => {
-        res.status(StatusCodes.NOT_FOUND).send({
-            message: ReasonPhrases.NOT_FOUND,
-            statusCode: StatusCodes.NOT_FOUND,
-        });
-    });
-
-    app.use(_logErrors);
-    app.use(_errorHandler);
+    // app.use((req, res) => {
+    //     res.status(StatusCodes.NOT_FOUND).send({
+    //         message: ReasonPhrases.NOT_FOUND,
+    //         statusCode: StatusCodes.NOT_FOUND,
+    //     });
+    // });
+    // app.use(_logErrors);
+    // app.use(_errorHandler);
 
     console.log('App successfully bootstrapped');
 
     return app;
 }
 
-
-function _logErrors(err, req, res, next) {
-    console.error(`ERROR occurred for request: ${req.originalUrl}`);
-    console.error(err.stack);
-    next(err);
-}
-
-function _errorHandler(err, req, res, next) {
-    res.status(err.code? err.code : StatusCodes.INTERNAL_SERVER_ERROR);
-    res.send(err.message);
+function addBooking(req, res) {
+    const booking = req.body;
+    bookings.push(booking);
+    res.send(booking);
 }

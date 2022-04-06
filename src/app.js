a5165b41b58c6cdcbc852862786454579ea219da
Borcea <cristian.borcea@sap.com>
2022-04-06 16:40:00 +0300

    moved business logic into app.js

--- src/app.js
+++ src/app.js
@@ -1,11 +1,12 @@
 import bodyParser from 'body-parser';
 import express from 'express';
-import routes from './routes/bookingsRoutes';
-import {StatusCodes, ReasonPhrases} from 'http-status-codes';
+import { body, oneOf, validationResult } from "express-validator";
+import { StatusCodes, ReasonPhrases } from 'http-status-codes';
+import { Booking } from './booking';
 
+const bookings = [];
 
 export const initializeApp = () => {
-
     const app = express();
 
     app.use(bodyParser.urlencoded({
@@ -14,11 +15,67 @@ export const initializeApp = () => {
 
     app.use(bodyParser.json());
 
-    app.get('/', (req, res) => res.send('App is working'));
+    app.get(`/api/bookings/:id`, function (req, res, next) {
+        console.log(`received view bookings request`);
+        try {
+            const id = req.params.id;
+            const booking = bookings.find(booking => booking.id == id);
+            // other service call (or same service, different functions can go here)
+            console.log("found booking....", booking);
+            if (booking) {
+                res.send(booking);
+                console.log(`found booking with id ${id}`);
+            } else {
+                console.log(`no booking found with id ${id}`);
+                res.sendStatus(StatusCodes.NOT_FOUND);
+            }
+        } catch (error) {
+            console.log(error.message);
+            next(error);
+        }
+    });
 
-    app.use('/api', routes);
+    app.post('/api/booking', async function (req, res, next) {
+        try {
+            const validationResults = await _validateRequest(req);
 
-    app.use((req, res) => {
+            if (!validationResults.isEmpty()) {
+                console.log("validation failed");
+                const errorMsg = validationResults.errors;
+                res
+                    .status(StatusCodes.BAD_REQUEST)
+                    .send(`validation done, error(s): ${JSON.stringify(errorMsg)}`);
+            } else {
+                console.log("there are no validation errors");
+
+                const {
+                    id,
+                    firstName,
+                    lastName,
+                    emailAddress,
+                    phoneNumber,
+                    flightDate,
+                    flightDestination
+                } = req.body;
+
+                const booking = new Booking(id);
+                booking.setPassenger(firstName, lastName, emailAddress, phoneNumber, flightDate, flightDestination);
+                bookings.push(booking);
+
+                // other service call (or same service, different functions can go here)
+                res.sendStatus(StatusCodes.CREATED);
+            }
+        } catch (error) {
+            console.log(error.message);
+            next(error);
+        }
+    });
+
+    app.get('/', function (req, res) {
+        res.send('App is working');
+    });
+
+    app.use(function (req, res) {
         res.status(StatusCodes.NOT_FOUND).send({
             message: ReasonPhrases.NOT_FOUND,
             statusCode: StatusCodes.NOT_FOUND,
@@ -33,6 +90,24 @@ export const initializeApp = () => {
     return app;
 }
 
+async function _validateRequest(req) {
+    await oneOf([
+        [
+            body("emailAddress", "Email address is mandatory").exists(),
+            body("phoneNumber", "Phone number is mandatory").exists(),
+        ],
+    ]).run(req);
+    await body(
+        ["firstName", "lastName"],
+        "First name and last name should be characters only"
+    )
+        .isAlpha()
+        .run(req);
+
+    const validationResults = validationResult(req);
+    console.info(`validation results: ${JSON.stringify(validationResults)}`);
+    return validationResults;
+};
 
 function _logErrors(err, req, res, next) {
     console.error(`ERROR occurred for request: ${req.originalUrl}`);
@@ -41,6 +116,6 @@ function _logErrors(err, req, res, next) {
 }
 
 function _errorHandler(err, req, res, next) {
-    res.status(err.code? err.code : StatusCodes.INTERNAL_SERVER_ERROR);
+    res.status(err.code ? err.code : StatusCodes.INTERNAL_SERVER_ERROR);
     res.send(err.message);
 }
